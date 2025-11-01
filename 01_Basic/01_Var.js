75ab225e03ba7821d8e935b5aafbceb41d6d40de
ITzBoTE <n0terr0r696969@gmail.com>
2025-11-01 15:24:21 +0530

    Var

---
+++ 01_Basic/01_Var.js
@@ -0,0 +1,19 @@
+const accountId = "1234567890abcdef"
+let accountEmail = "Nitesh@gmail.com"
+var accountPassword = "N@1234"
+accountCity = "Pune"
+
+// accountId = "0987654321fedcba"
+
+accountEmail = "Homezzyindia@gmail.com"
+accountPassword = "H@1234"
+accountCity = "Bangalore"
+
+// console.log(accountEmail);
+
+/*
+Not Prefer to use var keyword to declare variable
+because it creates confusion in block scope and function scope.
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
